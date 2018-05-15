a96a4a5a9dc22384e9b1e3681cc4eb7e1294f0c2
Flavien MICHALECZEK <f.michaleczek@arkadin.com>
2018-05-15 12:28:29 +0200

    Correct CLI.cmd executable locaiton

--- CLI.cmd
+++ CLI.cmd
@@ -1,22 +1,22 @@
 @ECHO OFF
 
-attrib.exe %WINDIR%\system32 -h | findstr.exe /I "system32" >nul
+%SystemRoot%\system32\attrib.exe %SystemRoot%\system32 -h | findstr.exe /I "system32" >nul
 IF %ERRORLEVEL% neq 1 (
-    powershell.exe -nol -nop -ex ByPass -WindowStyle Hidden -c "Start-Process '%0' -v RunAs"
+    %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -WindowStyle Hidden -Command "Start-Process '%0' -Verb RunAs"
 	EXIT /b %ERRORLEVEL%
 	GOTO :eof	
 )
 
 SET PWD=%~dp0
+
 SET TMP=%PWD%Temp
 SET TEMP=%PWD%Temp
-
 SET PSScriptRoot=%PWD:~0,-1%
 SET PSModulePath=%PSScriptRoot%\Modules;%PSModulePath%
 SET PSDisableModuleAnalysisCacheCleanup=1
 SET PSModuleAnalysisCachePath=%TEMP%\PSModuleAnalysisCache\%COMPUTERNAME%-%USERDOMAIN%-%USERNAME%.cache
 
-%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -NoExit -WindowStyle Maximized -Command "Set-Location '%PSScriptRoot%' ; Write-Host -Fore White -back DarkRed 'CLI' ;"
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -NoExit -WindowStyle Maximized -Command "Set-Location '%PSScriptRoot%' ; Write-Host -Foreground White -Background DarkRed 'PSAspNetCore CLI' ;"
 
 EXIT /b %ERRORLEVEL%
 GOTO :eof
